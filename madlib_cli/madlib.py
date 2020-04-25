34a9c633c7b97cd0332f0218ed521dc811b65a04
Chanyoon Zhu <zhqianyu@amazon.com>
2020-04-24 17:15:59 -0700

    creates mad libs game

---
+++ madlib_cli/madlib.py
@@ -0,0 +1,51 @@
+import re
+
+def parse_blanks(template):
+    blanks = []
+    regexp = r'\{.*?\}'
+    matches = re.findall(regexp, template)
+    for item in matches:
+        blanks.append(item)
+    return blanks
+
+def parse_original(template):
+    regexp = r'\{.*?\}'
+    return re.split(regexp, template)
+
+def get_input(blanks):
+    prompt = "Please type in a(n) "
+    inputs = []
+    for blank in blanks:
+        input_value = input(prompt + blank.lower() + ": ")
+        inputs.append(input_value)
+    return inputs
+
+def display(original_parsed, inputs):
+    output = []
+    for i in range(len(inputs)):
+        if i < len(original_parsed):
+            output.append(original_parsed[i])
+        output.append(inputs[i])
+    for i in range(len(inputs), len(original_parsed)):
+        output.append(original_parsed[i])
+    return ''.join(output)
+
+def read_file(filepath):
+    with open(filepath, 'r') as file:
+        return file.read()
+
+def write_to_file(content):
+    with open('assets/output.txt', 'w') as file:
+        file.write(content)
+    
+def main():
+    template = read_file('assets/template.txt')
+    blanks = parse_blanks(template)
+    original = parse_original(template)
+    inputs = get_input(blanks)
+    output = display(original, inputs)
+    write_to_file(output)
+
+if __name__ == "__main__":
+    main()
+
---
+++ tests/test_madlib.py
@@ -0,0 +1,52 @@
+from madlib_cli import __version__
+from madlib_cli.madlib import parse_blanks
+from madlib_cli.madlib import parse_original
+from madlib_cli.madlib import display
+from madlib_cli.madlib import read_file
+from madlib_cli.madlib import write_to_file
+
+def test_version():
+    assert __version__ == '0.1.0'
+
+def test_parse_blanks():
+    actual = parse_blanks("A {Adjective} and {Adjective} {Noun}")
+    expected = ["{Adjective}", "{Adjective}", "{Noun}"]
+    assert actual == expected
+
+def test_parse_original():
+    actual = parse_original("A {Adjective} and {Adjective} {Noun}")
+    expected = ["A ", " and ", " ", ""]
+    assert actual == expected
+
+def test_display_1():
+    original_parsed = ["A ", " and ", " ", ""]
+    inputs = ["dark", "stormy", "night"]
+    actual = "A dark and stormy night"
+    expected = display(original_parsed, inputs)
+    assert actual == expected
+
+def test_display_2():
+    original_parsed = ["A ", " and ", " ", " something"]
+    inputs = ["dark", "stormy", "night"]
+    actual = "A dark and stormy night something"
+    expected = display(original_parsed, inputs)
+    assert actual == expected
+
+def test_display_3():
+    original_parsed = ["A ", " and ", " "]
+    inputs = ["dark", "stormy", "night", "something"]
+    actual = "A dark and stormy nightsomething"
+    expected = display(original_parsed, inputs)
+    assert actual == expected
+
+def test_read_file():
+    actual = read_file('assets/template-test.txt')
+    expected = "A {Adjective} and {Adjective} {Noun}."
+    assert actual == expected
+
+def test_write_to_file():
+    expected = "content"
+    write_to_file(expected)
+    actual = read_file('assets/output.txt')
+    assert actual == expected
+    
